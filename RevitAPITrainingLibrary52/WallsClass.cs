b55e3c4fe54f87a59ef994806652539e15683130
Yovanovich.aa <Yovanovich.aa@DESKTOP-ITANAVJ>
2022-04-01 17:58:48 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingLibrary52/WallsClass.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary52
+{
+   public class WallsClass
+    {
+        public static List<WallType> GetWallTypes(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<WallType> wallTypes = new FilteredElementCollector(doc)
+                .OfClass(typeof(WallType))
+                .Cast<WallType>()
+                .ToList();
+            return wallTypes;
+        }
+    }
+}
